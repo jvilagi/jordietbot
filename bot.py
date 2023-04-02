38a2965595f44fae6e834deec5e3d6a209ac3904
Jordi <129681740+jvilagi@users.noreply.github.com>
2023-04-02 23:50:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,41 @@
-import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler, Filters, MessageHandler, Updater
 
-# Define a function that handles the /ababol command
-def ababol_handler(update: Update, context: CallbackContext):
-    # Define the buttons
-    button1 = InlineKeyboardButton('Button 1', callback_data='1')
-    button2 = InlineKeyboardButton('Button 2', callback_data='2')
-    button3 = InlineKeyboardButton('Button 3', callback_data='3')
-    buttons = [[button1, button2], [button3]]
+# Definim la funció per a la comanda /ababol
+def ababol(update, context):
+    # Creem els botons
+    buttons = [[InlineKeyboardButton("Botó 1", callback_data='button1'),
+                InlineKeyboardButton("Botó 2", callback_data='button2'),
+                InlineKeyboardButton("Botó 3", callback_data='button3')],
+               [InlineKeyboardButton("Botó 4", callback_data='button4'),
+                InlineKeyboardButton("Botó 5", callback_data='button5'),
+                InlineKeyboardButton("Botó 6", callback_data='button6')],
+               [InlineKeyboardButton("Botó 7", callback_data='button7'),
+                InlineKeyboardButton("Botó 8", callback_data='button8'),
+                InlineKeyboardButton("Botó 9", callback_data='button9')],
+               [InlineKeyboardButton("Botó 10", callback_data='button10')]]
 
-    # Send the message with the buttons
+    # Creem el markup amb els botons
     reply_markup = InlineKeyboardMarkup(buttons)
-    update.message.reply_text('Choose an option:', reply_markup=reply_markup)
 
-# Define a function that handles the button callbacks
-def button_callback(update: Update, context: CallbackContext):
+    # Enviem el missatge amb els botons
+    update.message.reply_text('Aquí tens els teus botons:', reply_markup=reply_markup)
+
+# Definim la funció per gestionar les respostes als botons
+def button(update, context):
     query = update.callback_query
     query.answer()
+    query.edit_message_text(text=f"Has premut el botó {query.data}")
 
-    # Get the button data
-    button_data = query.data
+# Configuració del bot i dels handlers
+TOKEN = "6011630982:AAF8viJgzYgtBaVTz-ETPPMrGMOnQTNu1eM"
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-    # Send a message with the button data
-    message = f'The button pressed was {button_data}.'
-    query.edit_message_text(text=message)
+# Afegim els handlers
+dispatcher.add_handler(CommandHandler("ababol", ababol))
+dispatcher.add_handler(CallbackQueryHandler(button))
 
-    # Remove the buttons
-    query.message.reply_text('Buttons have been removed.', reply_markup=InlineKeyboardMarkup([[]]))
-
-def main():
-    # Set up the logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-    # SAquí hi poso el TOKEN
-    updater = Updater('6011630982:AAF8viJgzYgtBaVTz-ETPPMrGMOnQTNu1eM', use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Add the command handler
-    dispatcher.add_handler(CommandHandler('ababol', ababol_handler))
-
-    # Add the callback query handler
-    dispatcher.add_handler(CallbackQueryHandler(button_callback))
-
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+# Iniciem el bot
+updater.start_polling()
