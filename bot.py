66f8144e8745fc339004982e80e9550f08dd3c44
Jordi <129681740+jvilagi@users.noreply.github.com>
2023-04-02 20:22:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,82 +1,32 @@
 import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, Filters, MessageHandler, Updater
 
+TOKEN = '6011630982:AAF8viJgzYgtBaVTz-ETPPMrGMOnQTNu1eM'
+
+# Estats per a la conversa amb el bot
+FIRST, SECOND = range(2)
+
+# Textos dels botons
+boton_text = ['Botó 1', 'Botó 2', 'Botó 3', 'Botó 4', 'Botó 5', 'Botó 6', 'Botó 7', 'Botó 8', 'Botó 9', 'Botó 10',
+              'Botó 11', 'Botó 12', 'Botó 13', 'Botó 14', 'Botó 15', 'Botó 16', 'Botó 17', 'Botó 18', 'Botó 19', 'Botó 20',
+              'Botó 21', 'Botó 22', 'Botó 23', 'Botó 24', 'Botó 25', 'Botó 26', 'Botó 27', 'Botó 28', 'Botó 29', 'Botó 30',
+              'Botó 31', 'Botó 32']
+
+# Textos associats a cada botó
+boton_info = ['Informació associada al botó 1', 'Informació associada al botó 2', 'Informació associada al botó 3',
+              'Informació associada al botó 4', 'Informació associada al botó 5', 'Informació associada al botó 6',
+              'Informació associada al botó 7', 'Informació associada al botó 8', 'Informació associada al botó 9',
+              'Informació associada al botó 10', 'Informació associada al botó 11', 'Informació associada al botó 12',
+              'Informació associada al botó 13', 'Informació associada al botó 14', 'Informació associada al botó 15',
+              'Informació associada al botó 16', 'Informació associada al botó 17', 'Informació associada al botó 18',
+              'Informació associada al botó 19', 'Informació associada al botó 20', 'Informació associada al botó 21',
+              'Informació associada al botó 22', 'Informació associada al botó 23', 'Informació associada al botó 24',
+              'Informació associada al botó 25', 'Informació associada al botó 26', 'Informació associada al botó 27',
+              'Informació associada al botó 28', 'Informació associada al botó 29', 'Informació associada al botó 30',
+              'Informació associada al botó 31', 'Informació associada al botó 32']
+
+# Funció que s'executa quan es rep el comandament /start
 def start(update, context):
-    keyboard = [
-        [
-            InlineKeyboardButton("Button 1", callback_data='1'),
-            InlineKeyboardButton("Button 2", callback_data='2'),
-            InlineKeyboardButton("Button 3", callback_data='3'),
-            InlineKeyboardButton("Button 4", callback_data='4')
-        ],
-        [
-            InlineKeyboardButton("Button 5", callback_data='5'),
-            InlineKeyboardButton("Button 6", callback_data='6'),
-            InlineKeyboardButton("Button 7", callback_data='7'),
-            InlineKeyboardButton("Button 8", callback_data='8')
-        ],
-        [
-            InlineKeyboardButton("Button 9", callback_data='9'),
-            InlineKeyboardButton("Button 10", callback_data='10'),
-            InlineKeyboardButton("Button 11", callback_data='11'),
-            InlineKeyboardButton("Button 12", callback_data='12')
-        ],
-        [
-            InlineKeyboardButton("Button 13", callback_data='13'),
-            InlineKeyboardButton("Button 14", callback_data='14'),
-            InlineKeyboardButton("Button 15", callback_data='15'),
-            InlineKeyboardButton("Button 16", callback_data='16')
-        ],
-        [
-            InlineKeyboardButton("Button 17", callback_data='17'),
-            InlineKeyboardButton("Button 18", callback_data='18'),
-            InlineKeyboardButton("Button 19", callback_data='19'),
-            InlineKeyboardButton("Button 20", callback_data='20')
-        ],
-        [
-            InlineKeyboardButton("Button 21", callback_data='21'),
-            InlineKeyboardButton("Button 22", callback_data='22'),
-            InlineKeyboardButton("Button 23", callback_data='23'),
-            InlineKeyboardButton("Button 24", callback_data='24')
-        ],
-        [
-            InlineKeyboardButton("Button 25", callback_data='25'),
-            InlineKeyboardButton("Button 26", callback_data='26'),
-            InlineKeyboardButton("Button 27", callback_data='27'),
-            InlineKeyboardButton("Button 28", callback_data='28')
-        ],
-        [
-            InlineKeyboardButton("Button 29", callback_data='29'),
-            InlineKeyboardButton("Button 30", callback_data='30'),
-            InlineKeyboardButton("Button 31", callback_data='31'),
-            InlineKeyboardButton("Button 32", callback_data='32')
-        ]
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-
-    button_number = query.data
-    text = f'You pressed button number {button_number}'
-
-    query.edit_message_text(text=text)
-
-
-def main():
-    token = '6011630982:AAF8viJgzYgtBaVTz-ETPPMrGMOnQTNu1eM'
-    updater = Updater(token, use_context=True)
-
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    # Es mostra un missatge d'inici i els botons
+    text = "Hola! Fes clic a un dels botons per obtenir més informació."
+    buttons = [[
